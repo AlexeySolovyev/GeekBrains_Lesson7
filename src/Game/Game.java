42b0aacd6e16a907f9806872d5e3fc1f520b0934
Alexey Solovyev <solovevalex@gmail.com>
2019-04-17 22:49:51 +0300

    Initial commit

---
+++ src/Game/Game.java
@@ -0,0 +1,15 @@
+package Game;
+
+public class Game {
+    private GameBoard board; // ссылка на игровое поле
+    private GamePlayer[] gamePlayers = new GamePlayer[2]; // массив игроков
+    private int playersTurn = 0; // индекс текущего игрока
+
+    public Game() {
+        this.board= new GameBoard(this);
+    }
+    public void initGame() {
+        gamePlayers[0] = new GamePlayer(true, 'X');
+        gamePlayers[1] = new GamePlayer(false, 'O');
+    }
+}
